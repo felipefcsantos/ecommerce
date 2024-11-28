d81c2f0aad50624deabfa768f45f7cac5cdd95e8
Felipe Santos <felipesantos_fcs98@hotmail.com>
2024-11-27 21:48:38 -0300

    fix: troca de display flex para grid na home

--- src/components/Produto/index.tsx
+++ src/components/Produto/index.tsx
@@ -4,6 +4,7 @@ import styles from "./styles.module.css";
 import AddShoppingCartIcon from "@mui/icons-material/AddShoppingCart";
 import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
 import FavoriteIcon from "@mui/icons-material/Favorite";
+import Grid from "@mui/material/Grid2";
 
 const Produto = ({ dados }: { dados: Produtos[] }) => {
   const { favoritarProduto } = useEcommerceContext();
@@ -21,48 +22,52 @@ const Produto = ({ dados }: { dados: Produtos[] }) => {
   };
 
   return (
-    <div className={styles.container}>
-      {dados.map((produto) => {
-        return (
-          <div key={produto.id} className={styles.produto}>
-            <div className={styles.container_img}>
-              <img
-                className={styles.img}
-                src={produto.image}
-                alt={produto.description}
-              />
-              <div className={styles.overlay}>
-                {handleVerificaFavorito(produto.id) ? (
-                  <FavoriteIcon
-                    className={styles.favorite}
-                    onClick={() => favoritarProduto(produto.id)}
-                  />
-                ) : (
-                  <FavoriteBorderIcon
-                    className={styles.favorite}
-                    onClick={() => favoritarProduto(produto.id)}
-                  />
-                )}
-                <button className={styles.addToCartButton}>
-                  Add to cart
-                  <AddShoppingCartIcon sx={{ marginLeft: ".5rem" }} />
-                </button>
+    <>
+      <Grid container spacing={2}>
+        {dados.map((produto) => {
+          return (
+            <Grid size={4} key={produto.id}>
+            <div className={styles.produto}>
+              <div className={styles.container_img}>
+                <img
+                  className={styles.img}
+                  src={produto.image}
+                  alt={produto.description}
+                />
+                <div className={styles.overlay}>
+                  {handleVerificaFavorito(produto.id) ? (
+                    <FavoriteIcon
+                      className={styles.favorite}
+                      onClick={() => favoritarProduto(produto.id)}
+                    />
+                  ) : (
+                    <FavoriteBorderIcon
+                      className={styles.favorite}
+                      onClick={() => favoritarProduto(produto.id)}
+                    />
+                  )}
+                  <button className={styles.addToCartButton}>
+                    Add to cart
+                    <AddShoppingCartIcon sx={{ marginLeft: ".5rem" }} />
+                  </button>
+                </div>
+              </div>
+              <hr />
+              <div className={styles.title}>
+                <p>{produto.title}</p>
+              </div>
+              <div className={styles.container_price}>
+                <span>
+                  <p>IN STOCK</p>
+                </span>
+                <p className={styles.price}>R$ {produto.price}</p>
               </div>
             </div>
-            <hr />
-            <div className={styles.title}>
-              <p>{produto.title}</p>
-            </div>
-            <div className={styles.container_price}>
-              <span>
-                <p>IN STOCK</p>
-              </span>
-              <p className={styles.price}>R$ {produto.price}</p>
-            </div>
-          </div>
-        );
-      })}
-    </div>
+            </Grid>
+          );
+        })}
+      </Grid>
+    </>
   );
 };
 
